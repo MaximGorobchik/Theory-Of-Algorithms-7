93fd83ef143349528eda817bb423187dfbd0c8e2
МАКСИМ <@FSB>
2023-01-04 22:12:38 +0200

    Добавьте файлы проекта.

---
+++ Теорія Алгоритмів лаба 7/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введiть N: "); var N = int.Parse(Console.ReadLine());
+Random rand = new Random();
+int[]array = new int[N];
+var writer = new StreamWriter(File.OpenWrite("result.txt"));
+Console.WriteLine($"Результат: ");
+Console.ForegroundColor= ConsoleColor.Red;
+for (int i = 0; i < N; i++)
+{
+    array[i] = rand.Next(100);
+    Console.Write($"{array[i]} ");
+    writer.Write($"{array[i]} ");
+}
+Console.ForegroundColor= ConsoleColor.White;
+Console.Write($"{Environment.NewLine}Введiть число для перевiрки на входження: "); var num = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i=0;i < N;i++)
+{
+    if (array[i] == num)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"{num} - {count}");
+writer.WriteLine($"{Environment.NewLine}{num} - {count}");
+writer.Close();
